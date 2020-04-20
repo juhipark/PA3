588362e73bc78015067c1f9c6fcc117c83282bcb
None <None>
2020-04-19 21:47:17 -0700

    Creating host for each subnet

--- legacy_router.py
+++ legacy_router.py
@@ -14,31 +14,33 @@ def myNetwork():
 
     net = Mininet( topo=None,
                    build=False,
-                   ipBase='192.168.56.1/24')
+                   ipBase='10.0.0.0/8')
 
-    info( '*** Adding controller\n' )
-    info( '*** Add switches\n')
-    r1 = net.addHost('r1', cls=Node, ip='192.168.56.1/24')
+    # for example '-.-.-.-/24'is ip addfress format to set prefix length 
+    info( '*** Add Router\n')
+    r1 = net.addHost('r1', cls=Node, ip='10.0.0.1')
     r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
+    info('*** Add Switch\n')
+    switch = net.addSwitch('s1')
+
     info( '*** Add hosts\n')
-    h1 = net.addHost('h1', cls=Host, ip='10.0.2.15/24', defaultRoute='via 10.0.2.1')
-    h2 = net.addHost('h2', cls=Host, ip='127.0.0.1/24', defaultRoute='via 127.0.0.1')
+    h1 = net.addHost('h1', cls=Host, ip='10.0.0.3/8', defaultRoute='via 10.0.0.1')
+    h2 = net.addHost('h2', cls=Host, ip='192.0.1.2/12', defaultRoute='via 192.0.1.1')
 
     info( '*** Add links\n')
-    net.addLink(r1, h1, intfName2='r1-eth1', params2={'ip':'10.0.2.1/24'})
-    net.addLink(r1, h2, intfName2='r1-eth2', params2={'ip':'127.0.0.1/24'})
+    net.addLink(h1, r1, intfName2='r1-eth1', params2={'ip':'10.0.0.1/8'})
+    net.addLink(h2, r1, intfName2='r1-eth2', params2={'ip':'192.0.1.1/12'})
 
     info( '*** Starting network\n')
     net.build()
-    info( '*** Starting controllers\n')
+    
+    info('*** Starting controllers\n')
     for controller in net.controllers:
         controller.start()
 
-    info( '*** Starting switches\n')
-
-    info( '*** Post configure switches and hosts\n')
-
+    info('*** Starting switches\n')
+    
     CLI(net)
     net.stop()
 
